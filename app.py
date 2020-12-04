ddc1e078d36535326d38a6f68c976ef7af80d540
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2020-12-04 02:58:52 -0700

    Database setup

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy.pool import StaticPool
+
+#################################################
+# Database Setup
+#################################################
+# Reference: https://stackoverflow.com/questions/33055039/using-sqlalchemy-scoped-session-in-theading-thread
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={"check_same_thread": False}, poolclass=StaticPool, echo=True)
+
