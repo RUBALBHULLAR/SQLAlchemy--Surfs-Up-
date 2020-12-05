b4e84729803eaeebb0ba63220c39eca3b902dc66
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2020-12-05 02:29:25 -0700

    update climate analysis

--- app.py
+++ app.py
@@ -18,3 +18,77 @@ from sqlalchemy.pool import StaticPool
 # Reference: https://stackoverflow.com/questions/33055039/using-sqlalchemy-scoped-session-in-theading-thread
 engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={"check_same_thread": False}, poolclass=StaticPool, echo=True)
 
+# Reflect Existing Database Into a New Model
+Base = automap_base()
+# Reflect the Tables
+Base.prepare(engine, reflect=True)
+
+# Save References to Each Table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create Session (Link) From Python to the DB
+session = Session(engine)
+
+#Setup Flask
+app = Flask(__name__)
+
+#Set Flask Routes
+
+@app.route("/")
+def main():
+    """List all routes that are available."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the JSON representation of your dictionary."""
+
+    print("Received precipitation api request.")
+
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+
+    
+    begin_date = max_date - dt.timedelta(365)
+
+    
+    precipitation_data = session.query(func.strftime("%Y-%m-%d", Measurement.date), Measurement.prcp).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) >= begin_date).all()
+    
+
+    results_dict = {}
+    for result in precipitation_data:
+        results_dict[result[0]] = result[1]
+
+    return jsonify(results_dict)
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a JSON list of stations from the dataset."""
+
+    print("Received station api request.")
+
+    #query stations list
+    stations = session.query(Station).all()
+
+    #create a list of dictionaries
+    stations_list = []
+    for station in stations:
+        station_dict = {}
+        station_dict["id"] = station.id
+        station_dict["station"] = station.station
+        station_dict["name"] = station.name
+        station_dict["latitude"] = station.latitude
+        station_dict["longitude"] = station.longitude
+        station_dict["elevation"] = station.elevation
+        stations_list.append(station_dict)
+
+    return jsonify(stations_list)
